c6f9284c43cf236294a96a40878c2f6c6ce2a023
Etienne Tremel <etremel@travix.com>
2015-11-27 22:20:21 +0100

    Make service discovery run.sh script automated, fix typo

--- proxy/run.sh
+++ proxy/run.sh
@@ -1,10 +1,41 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Startup script, replace all {{VARIABLE}} to correct Service IP/Host
-#
+
 set -e
 
-sed -i "s/{{SITE_ONE_SERVICE}}/${SITE_ONE_SERVICE_HOST}:${SITE_ONE_SERVICE_PORT}/g;" /etc/nginx/sites-enabled/site-one.conf
-sed -i "s/{{SITE_TWO_SERVICE}}/${SITE_TWO_SERVICE_HOST}:${SITE_TWO_SERVICE_PORT}/g;" /etc/nginx/sites-enabled/site-two.conf
+# Make sure NGINX is not breaking because service is not available
+default_service_port="8080"
+default_service_host="127.0.0.1"
+configuration_files="/etc/nginx/sites-enabled/*.conf"
+
+for file in $configuration_files
+do
+  placeholder_array=$(grep -Eo "(\{\{[A-Z_]+_SERVICE\}\})" $file)
+
+  for placeholder in $placeholder_array
+  do
+    service_name=$(echo "$placeholder" | sed -n 's/{{\([A-Z_]*\)_SERVICE}}/\1/p')
+
+    host="${service_name}_SERVICE_HOST"
+    port="${service_name}_SERVICE_PORT"
+
+		# Check if environment variable is available, otherwise set default
+    if [ -z ${!host} ]; then
+      service_host=$default_service_host
+    else
+      service_host=${!host}
+    fi
+
+    if [ -z ${!port} ]; then
+      service_port=$default_service_port
+    else
+      service_port=${!port}
+    fi
+
+    # Search and replace service variable in upstream with correct Host IP/Port
+    sed -i "s/${placeholder}/${service_host}:${service_port}/g;" $file
+  done
+done
 
 exec "$@"
